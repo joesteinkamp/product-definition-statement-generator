5bf5d1e8b1c255530d72892ac2164ac471baa6ca
Joe Steinkamp <joe@joesteinkamp.com>
2019-08-04 14:57:14 -0500

    Style (mostly selection) updates

--- src/App.js
+++ src/App.js
@@ -13,6 +13,12 @@ class App extends Component {
       selectedObstacle: '', 
       selectedNegFeel: '', 
       completeData: false,
+      rowActivity1Active: false,
+      rowActivity2Active: false,
+      rowActivity3Active: false,
+      rowStep1Active: false,
+      rowStep2Active: false,
+      rowStep3Active: false
     };
   }
 
@@ -29,11 +35,17 @@ class App extends Component {
     const { activity } = this.formActivity;
     console.log(activity.value);
 
+    // Toggle row's active state
+
+
     if (activity.value === "act1") {
       this.setState(
         {
           selectedActivity: this.activity1.value,
-          selectedReason: this.reason1.value
+          selectedReason: this.reason1.value,
+          rowActivity1Active: true,
+          rowActivity2Active: false,
+          rowActivity3Active: false
         }
       , () => { this.checkDataIsComplete(); });
     }
@@ -41,7 +53,10 @@ class App extends Component {
       this.setState(
         {
           selectedActivity: this.activity2.value,
-          selectedReason: this.reason2.value
+          selectedReason: this.reason2.value,
+          rowActivity1Active: false,
+          rowActivity2Active: true,
+          rowActivity3Active: false
         }
       , () => { this.checkDataIsComplete(); });
     }
@@ -49,7 +64,10 @@ class App extends Component {
       this.setState(
         {
           selectedActivity: this.activity3.value,
-          selectedReason: this.reason3.value
+          selectedReason: this.reason3.value,
+          rowActivity1Active: false,
+          rowActivity2Active: false,
+          rowActivity3Active: true
         }
       , () => { this.checkDataIsComplete(); });
     }
@@ -67,7 +85,10 @@ class App extends Component {
         {
           selectedStep: this.step1.value,
           selectedObstacle: this.obstacle1.value,
-          selectedNegFeel: this.negFeel1.value
+          selectedNegFeel: this.negFeel1.value,
+          rowStep1Active: true,
+          rowStep2Active: false,
+          rowStep3Active: false
         }
       , () => { this.checkDataIsComplete(); });
     }
@@ -76,7 +97,10 @@ class App extends Component {
         {
           selectedStep: this.step2.value,
           selectedObstacle: this.obstacle2.value,
-          selectedNegFeel: this.negFeel2.value
+          selectedNegFeel: this.negFeel2.value,
+          rowStep1Active: false,
+          rowStep2Active: true,
+          rowStep3Active: false
         }
       , () => { this.checkDataIsComplete(); });
     }
@@ -85,7 +109,10 @@ class App extends Component {
         {
           selectedStep: this.step3.value,
           selectedObstacle: this.obstacle3.value,
-          selectedNegFeel: this.negFeel3.value
+          selectedNegFeel: this.negFeel3.value,
+          rowStep1Active: false,
+          rowStep2Active: false,
+          rowStep3Active: true
         }
       , () => { this.checkDataIsComplete(); });
     }
@@ -97,6 +124,13 @@ class App extends Component {
       this.setState(
         {
           completeData: true
+        }
+      );
+    }
+    else {
+      this.setState(
+        {
+          completeData: false
         }
       );
     }
@@ -113,216 +147,220 @@ class App extends Component {
       output = <FormOutput userRole={this.state.userRole} selectedReason={this.state.selectedReason} selectedStep={this.state.selectedStep} selectedNegFeel={this.state.selectedNegFeel} selectedObstacle={this.state.selectedObstacle} />;
     }
     else {
-      output = '';
+      output = <pre>Fill out and select the primary activity and step you want to focus on.</pre>;
     }
 
 
     return (
       <div className="App">
         <h1>Problem Statement Generator</h1>
-        <h2>Who Is The User</h2>
-        <label htmlFor="input-user">
-          User Role (Persona)
-          <input 
-            id="input-user"
-            type="text"
-            value={this.state.userRole}
-            onChange={(i) => this.handleChangeUser(i)}
-          />
-        </label>
+        <div className="section">
+          <h2>Who Is The User</h2>
+          <label htmlFor="input-user">
+            User Role (Persona)
+            <input 
+              id="input-user"
+              type="text"
+              value={this.state.userRole}
+              onChange={(i) => this.handleChangeUser(i)}
+            />
+          </label>
+        </div>
 
-        <h2>Key Activities Performed by User</h2>
-        <p>Activity = a task that the user performs. Reason = user’s root goal and the why they’re doing the activity.</p>
+        <div className="section">
+          <h2>Key Activities Performed by User</h2>
+          <p>Activity = a task that the user performs. Reason = user’s root goal and the why they’re doing the activity.</p>
 
-        <form id="formActivity" ref={form => this.formActivity = form}>
-          <div className="grid rowActivity">
-            <label htmlFor="input-activity-1" className="col">
-              Activity
+          <form id="formActivity" ref={form => this.formActivity = form}>
+            <div className={this.state.rowActivity1Active ? "grid rowActivity active" : "grid rowActivity"}>
+              <label htmlFor="input-activity-1" className="col">
+                Activity
+                <input 
+                  id="input-activity-1"
+                  type="text"
+                  ref={input => this.activity1 = input}
+                />
+              </label>
+              <label htmlFor="input-reason-1" className="col">
+                Reason
+                <input 
+                  id="input-reason-1"
+                  type="text"
+                  ref={input => this.reason1 = input}
+                />
+              </label>
               <input 
-                id="input-activity-1"
-                type="text"
-                ref={input => this.activity1 = input}
+                id="input-radio-act1" 
+                className="col"
+                type="radio" 
+                value="act1" 
+                name="activity" 
+                onClick={(i) => this.handleActivitySelect(i)} 
               />
-            </label>
-            <label htmlFor="input-reason-1" className="col">
-              Reason
+            </div>
+            <div className={this.state.rowActivity2Active ? "grid rowActivity active" : "grid rowActivity"}>
+              <label htmlFor="input-activity-2" className="col">
+                Activity
+                <input 
+                  id="input-activity-2"
+                  type="text"
+                  ref={input => this.activity2 = input}
+                />
+              </label>
+              <label htmlFor="input-reason-2" className="col">
+                Reason
+                <input 
+                  id="input-reason-2"
+                  type="text"
+                  ref={input => this.reason2 = input}
+                />
+              </label>
               <input 
-                id="input-reason-1"
-                type="text"
-                ref={input => this.reason1 = input}
+                id="input-radio-act2" 
+                className="col"
+                type="radio" 
+                value="act2" 
+                name="activity" 
+                onClick={(i) => this.handleActivitySelect(i)} 
               />
-            </label>
-            <input 
-              id="input-radio-act1" 
-              className="col"
-              type="radio" 
-              value="act1" 
-              name="activity" 
-              onChange={(i) => this.handleActivitySelect(i)} 
-            />
-          </div>
-          <div className="grid rowActivity">
-            <label htmlFor="input-activity-2" className="col">
-              Activity
+            </div>
+            <div className={this.state.rowActivity3Active ? "grid rowActivity active" : "grid rowActivity"}>
+              <label htmlFor="input-activity-3" className="col">
+                Activity
+                <input 
+                  id="input-activity-3"
+                  type="text"
+                  ref={input => this.activity3 = input}
+                />
+              </label>
+              <label htmlFor="input-reason-2" className="col">
+                Reason
+                <input 
+                  id="input-reason-3"
+                  type="text"
+                  ref={input => this.reason3 = input}
+                />
+              </label>
               <input 
-                id="input-activity-2"
-                type="text"
-                ref={input => this.activity2 = input}
+                id="input-radio-act3" 
+                className="col"
+                type="radio" 
+                value="act3" 
+                name="activity" 
+                onClick={(i) => this.handleActivitySelect(i)} 
               />
-            </label>
-            <label htmlFor="input-reason-2" className="col">
-              Reason
-              <input 
-                id="input-reason-2"
-                type="text"
-                ref={input => this.reason2 = input}
-              />
-            </label>
-            <input 
-              id="input-radio-act2" 
-              className="col"
-              type="radio" 
-              value="act2" 
-              name="activity" 
-              onChange={(i) => this.handleActivitySelect(i)} 
-            />
-          </div>
-          <div className="grid rowActivity">
-            <label htmlFor="input-activity-3" className="col">
-              Activity
-              <input 
-                id="input-activity-3"
-                type="text"
-                ref={input => this.activity3 = input}
-              />
-            </label>
-            <label htmlFor="input-reason-2" className="col">
-              Reason
-              <input 
-                id="input-reason-3"
-                type="text"
-                ref={input => this.reason3 = input}
-              />
-            </label>
-            <input 
-              id="input-radio-act3" 
-              className="col"
-              type="radio" 
-              value="act3" 
-              name="activity" 
-              onChange={(i) => this.handleActivitySelect(i)} 
-            />
-          </div>
-        </form>
+            </div>
+          </form>
+        </div>
         
-        
+        <div className="section">
+          <h2>Specific Steps To Complete Activity</h2>
 
-        <h2>Specific Steps To Complete Activity</h2>
-
-        <form id="formStep" ref={form => this.formStep = form}>
-          <div className="grid rowStep">
-            <label htmlFor="input-step-1" className="col">
-              Step
+          <form id="formStep" ref={form => this.formStep = form}>
+            <div className={this.state.rowStep1Active ? "grid rowStep active" : "grid rowStep"}>
+              <label htmlFor="input-step-1" className="col">
+                Step
+                <input 
+                  id="input-step-1"
+                  type="text"
+                  ref={input => this.step1 = input}
+                />
+              </label>
+              <label htmlFor="input-obstacle-1" className="col">
+                Obstacle
+                <input 
+                  id="input-obstacle-1"
+                  type="text"
+                  ref={input => this.obstacle1 = input}
+                />
+              </label>
+              <label htmlFor="input-negfeel-1" className="col">
+                Negative Feeling
+                <input 
+                  id="input-negfeel-1"
+                  type="text"
+                  ref={input => this.negFeel1 = input}
+                />
+              </label>
               <input 
-                id="input-step-1"
-                type="text"
-                ref={input => this.step1 = input}
+                id="input-radio-step1" 
+                className="col"
+                type="radio" 
+                value="step1" 
+                name="step" 
+                onClick={(i) => this.handleStepSelect(i)} 
               />
-            </label>
-            <label htmlFor="input-obstacle-1" className="col">
-              Obstacle
+            </div>
+            <div className={this.state.rowStep2Active ? "grid rowStep active" : "grid rowStep"}>
+              <label htmlFor="input-step-2" className="col">
+                Step
+                <input 
+                  id="input-step-2"
+                  type="text"
+                  ref={input => this.step2 = input}
+                />
+              </label>
+              <label htmlFor="input-obstacle-2" className="col">
+                Obstacle
+                <input 
+                  id="input-obstacle-2"
+                  type="text"
+                  ref={input => this.obstacle2 = input}
+                />
+              </label>
+              <label htmlFor="input-negfeel-2" className="col">
+                Negative Feeling
+                <input 
+                  id="input-negfeel-2"
+                  type="text"
+                  ref={input => this.negFeel2 = input}
+                />
+              </label>
               <input 
-                id="input-obstacle-1"
-                type="text"
-                ref={input => this.obstacle1 = input}
+                id="input-radio-step2" 
+                className="col"
+                type="radio" 
+                value="step2" 
+                name="step" 
+                onClick={(i) => this.handleStepSelect(i)} 
               />
-            </label>
-            <label htmlFor="input-negfeel-1" className="col">
-              Negative Feeling
+            </div>
+            <div className={this.state.rowStep3Active ? "grid rowStep active" : "grid rowStep"}>
+              <label htmlFor="input-step-3" className="col">
+                Step
+                <input 
+                  id="input-step-3"
+                  type="text"
+                  ref={input => this.step3 = input}
+                />
+              </label>
+              <label htmlFor="input-obstacle-3" className="col">
+                Obstacle
+                <input 
+                  id="input-obstacle-3"
+                  type="text"
+                  ref={input => this.obstacle3 = input}
+                />
+              </label>
+              <label htmlFor="input-negfeel-3" className="col">
+                Negative Feeling
+                <input 
+                  id="input-negfeel-3"
+                  type="text"
+                  ref={input => this.negFeel3 = input}
+                />
+              </label>
               <input 
-                id="input-negfeel-1"
-                type="text"
-                ref={input => this.negFeel1 = input}
+                id="input-radio-step3" 
+                className="col"
+                type="radio" 
+                value="step3" 
+                name="step" 
+                onClick={(i) => this.handleStepSelect(i)} 
               />
-            </label>
-            <input 
-              id="input-radio-step1" 
-              className="col"
-              type="radio" 
-              value="step1" 
-              name="step" 
-              onChange={(i) => this.handleStepSelect(i)} 
-            />
-          </div>
-          <div className="grid rowStep">
-            <label htmlFor="input-step-2" className="col">
-              Step
-              <input 
-                id="input-step-2"
-                type="text"
-                ref={input => this.step2 = input}
-              />
-            </label>
-            <label htmlFor="input-obstacle-2" className="col">
-              Obstacle
-              <input 
-                id="input-obstacle-2"
-                type="text"
-                ref={input => this.obstacle2 = input}
-              />
-            </label>
-            <label htmlFor="input-negfeel-2" className="col">
-              Negative Feeling
-              <input 
-                id="input-negfeel-2"
-                type="text"
-                ref={input => this.negFeel2 = input}
-              />
-            </label>
-            <input 
-              id="input-radio-step2" 
-              className="col"
-              type="radio" 
-              value="step2" 
-              name="step" 
-              onChange={(i) => this.handleStepSelect(i)} 
-            />
-          </div>
-          <div className="grid rowStep">
-            <label htmlFor="input-step-3" className="col">
-              Step
-              <input 
-                id="input-step-3"
-                type="text"
-                ref={input => this.step3 = input}
-              />
-            </label>
-            <label htmlFor="input-obstacle-3" className="col">
-              Obstacle
-              <input 
-                id="input-obstacle-3"
-                type="text"
-                ref={input => this.obstacle3 = input}
-              />
-            </label>
-            <label htmlFor="input-negfeel-3" className="col">
-              Negative Feeling
-              <input 
-                id="input-negfeel-3"
-                type="text"
-                ref={input => this.negFeel3 = input}
-              />
-            </label>
-            <input 
-              id="input-radio-step3" 
-              className="col"
-              type="radio" 
-              value="step3" 
-              name="step" 
-              onChange={(i) => this.handleStepSelect(i)} 
-            />
-          </div>
-        </form>
+            </div>
+          </form>
+        </div>
 
         {/* <button onClick={(i) => this.handleSubmit(i)}>
 					Generate Problem Statement
@@ -336,11 +374,7 @@ class App extends Component {
 
 function FormOutput(props) {
 	return (
-		<div className="output">
-      <h2>Problem Statement</h2>
-      <p>Select the primary activity and step you want to focus on.</p>
-      <pre>A {props.userRole} who wants to {props.selectedReason} needs to {props.selectedStep} but feels {props.selectedNegFeel} about {props.selectedObstacle}.</pre>
-    </div>
+    <pre>A {props.userRole} who wants to {props.selectedReason} needs to {props.selectedStep} but feels {props.selectedNegFeel} about {props.selectedObstacle}.</pre>
 	);
 }
 
